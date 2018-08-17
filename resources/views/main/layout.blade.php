6b930dc004c5a351b3e9beda282a3820a6777f93
adizbek <adizbek1998@gmail.com>
2018-08-17 12:58:11 +0500

    texts

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -3,22 +3,22 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Tour Template</title>
+    <title>Travel to Uzbekistan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content=""/>
-    <meta name="keywords" content=""/>
-    <meta name="author" content=""/>
+    <meta name="description" content="Travel to Uzbekistan"/>
+    <meta name="keywords" content="Travel to Uzbekistan, travel, uzbekistan, tour"/>
+    <meta name="author" content="@adizbek"/>
 
     <!-- Facebook and Twitter integration -->
-    <meta property="og:title" content=""/>
-    <meta property="og:image" content=""/>
-    <meta property="og:url" content=""/>
-    <meta property="og:site_name" content=""/>
-    <meta property="og:description" content=""/>
-    <meta name="twitter:title" content=""/>
-    <meta name="twitter:image" content=""/>
-    <meta name="twitter:url" content=""/>
-    <meta name="twitter:card" content=""/>
+    <meta property="og:title" content="Travel to Uzbekistan"/>
+    <meta property="og:image" content="{{url("/images/uzb-1.jpg"}}"/>
+    <meta property="og:url" content="{{url("/images/uzb-1.jpg"}}"/>
+    <meta property="og:site_name" content="Travel to Uzbekistan"/>
+    <meta property="og:description" content="Travel to Uzbekistan"/>
+    <meta name="twitter:title" content="Travel to Uzbekistan"/>
+    <meta name="twitter:image" content="{{url("/images/uzb-1.jpg"}}"/>
+    <meta name="twitter:url" content="{{url("/images/uzb-1.jpg"}}"/>
+    {{--<meta name="twitter:card" content=""/>--}}
 
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
 
@@ -76,17 +76,18 @@
                                     @foreach($cities as $city)
                                         <li><a href="#">{{$city->name}}</a></li>
                                     @endforeach
-                                </ul>
-                            </li>
-                            <li @yield('nav.hotels')><a href="/hotels">Отели</a></li>
-                            <li @yield('nav.monuments')><a href="/monuments">Памятники</a></li>
-                            <li @yield('nav.news')><a href="/news">Новости</a></li>
-                            <li @yield('nav.contacts')><a href="/contact">Контакт</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+            </ul>
+        </li>
+        <li @yield('nav.hotels')><a href="
+    /hotels">Отели</a></li>
+    <li @yield('nav.monuments')><a href="/monuments">Памятники</a></li>
+    <li @yield('nav.news')><a href="/news">Новости</a></li>
+    <li @yield('nav.contacts')><a href="/contact">Контакт</a></li>
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
     </nav>
 
     @yield('content')
@@ -111,9 +112,8 @@
         <div class="container">
             <div class="row row-pb-md">
                 <div class="col-md-3 colorlib-widget">
-                    <h4>Tour Agency</h4>
-                    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta
-                        adipisci architecto culpa amet.</p>
+                    <h4>Travel to Uzbekistan</h4>
+                    <p>Special for <a href="http://vci.venkon.uz">vci.venkon.uz</a></p>
                     <p>
                     <ul class="colorlib-social-icons">
                         <li><a href="https://t.me/Adizbek"><i class="icon-telegram"></i></a></li>
@@ -154,36 +154,36 @@
 
         </div>
     </footer>
-</div>
+    </div>
 
 
-<div class="gototop js-top">
-    <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
-</div>
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+    </div>
 
-<!-- jQuery -->
-<script src="/js/jquery.min.js"></script>
-<!-- jQuery Easing -->
-<script src="/js/jquery.easing.1.3.js"></script>
-<!-- Bootstrap -->
-<script src="/js/bootstrap.min.js"></script>
-<!-- Waypoints -->
-<script src="/js/jquery.waypoints.min.js"></script>
-<!-- Flexslider -->
-<script src="/js/jquery.flexslider-min.js"></script>
-<!-- Owl carousel -->
-<script src="/js/owl.carousel.min.js"></script>
-<!-- Magnific Popup -->
-<script src="/js/jquery.magnific-popup.min.js"></script>
-<script src="/js/magnific-popup-options.js"></script>
-<!-- Date Picker -->
-<script src="/js/bootstrap-datepicker.js"></script>
-<!-- Stellar Parallax -->
-<script src="/js/jquery.stellar.min.js"></script>
+    <!-- jQuery -->
+    <script src="/js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="/js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="/js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="/js/jquery.flexslider-min.js"></script>
+    <!-- Owl carousel -->
+    <script src="/js/owl.carousel.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="/js/jquery.magnific-popup.min.js"></script>
+    <script src="/js/magnific-popup-options.js"></script>
+    <!-- Date Picker -->
+    <script src="/js/bootstrap-datepicker.js"></script>
+    <!-- Stellar Parallax -->
+    <script src="/js/jquery.stellar.min.js"></script>
 
-<!-- Main -->
-<script src="/js/main.js"></script>
+    <!-- Main -->
+    <script src="/js/main.js"></script>
 
-</body>
+    </body>
 </html>
 
